5cf11f1a9d1f4f368ad3aa0741e9d5762670f6e9
unknown <sharmadhananjay1904@gmail.com>
2022-09-27 22:26:20 +0530

    Pass, Continue & Break

---
+++ Control_Flow/Pass_Continue_Break.py
@@ -0,0 +1,13 @@
+#Pass basically ignores an line of code to which It's related to, specifically IF-ELSE Clauses, Functions or Loops
+for i in range (1, 101):
+    pass
+
+#A Break statement comes out of the IF-ELSE Clause or Loop whenever mentioned in these clauses
+for i in range(1, 7):
+    print(i)
+    if i == 4:
+        break
+#In this case, There will be no numbers printed after 4
+
+
+
