58254e049d74d9bdbb2b1b853179fe204f247d27
unknown <sharmadhananjay1904@gmail.com>
2022-09-27 22:54:41 +0530

    New Edit

---
+++ Control_Flow/Functions.py
@@ -0,0 +1,10 @@
+
+def greeting():
+    print("Hola! Hope you're doing great!")
+
+greeting()
+
+def addition(a, b, c):
+    print(a + b + c)
+
+addition(23, 23, 432)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+# Looking after Monty the Python!
 A Repository dedicated to all kinds of stuff Related to Python!
