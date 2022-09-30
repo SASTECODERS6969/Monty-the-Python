2da873962169805fe2d26c925578ae8e25e1ee8f
unknown <sharmadhananjay1904@gmail.com>
2022-09-30 20:00:04 +0530

    Problems on Loops

---
+++ Problems/FOR_LOOPS/q1.py
@@ -0,0 +1,3 @@
+#WAP TO PRINT THE FIRST 10 NATURAL NUMBERS
+for i in range(1, 11):
+    print(i)
---
+++ Problems/FOR_LOOPS/q2.py
@@ -0,0 +1,5 @@
+#WAP TO FIND THE SUM OF NUMBERS FROM 1 TO ANY NUMBER
+a = 0
+for i in range(1, 10):
+    a += i
+    print(i)
